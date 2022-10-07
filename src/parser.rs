235d63aada7d5b36b8e7d7937428240f17e15e9c
井澤伊織 <izawa.iori.tan@gmail.com>
2022-10-07 11:58:20 -0400

    fix typo

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ const META:      usize = 0b00000011;
 // Byte Structure
 // NEW_LAYER number_of_nodes (NEW_NODE node_type number_of_weights bias (weight)*number_of_weights)*number_of_nodes
 // META number_of_input_nodes cst_size cst alpha
-// EXP number_of_points ((value)*number_of_input_nodes)*number_of_points (value)*number_of_points
+// EXP number_of_points ((value)*number_of_input_nodes)*number_of_points (value)*number_of_points)
 // all vars are float: [u8; 4]
 // (x)*b means (x, x, x, ...) b times.
 
